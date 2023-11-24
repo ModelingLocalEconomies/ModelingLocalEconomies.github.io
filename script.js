1973f80717f2998206350babd4813e7d2b5d8752
raseen3 <raseen3@vt.edu>
2023-11-24 15:22:23 -0500

    made minor changes

---
+++ script.js
@@ -0,0 +1,8 @@
+// window.addEventListener('scroll', function() {
+//     var header = document.getElementById('main-header');
+//     if (window.scrollY > 5) { // Adjust this value based on when you want the transition to happen
+//         header.classList.add('navbar');
+//     } else {
+//         header.classList.remove('navbar');
+//     }
+// });
